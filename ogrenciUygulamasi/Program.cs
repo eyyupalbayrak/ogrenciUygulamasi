dfb8b46f576bd90b1262f69259b20c9fb6292017
eyyup albayrak <galacos-69@hotmail.com>
2021-03-18 12:33:03 +0300

    Add project files.

---
+++ ogrenciUygulamasi/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrenciUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            ogrencinin ogrencinin = new ogrencinin(11,"eyyup","albayrak",60,80,60,"istanbul üniversitesi");
+            
+            
+
+
+            while (kontrol)
+            {
+                islemleriGöster();
+                string secim = Console.ReadLine();
+                switch (secim)
+                {
+                    case "1":
+                        ogrencinin.ogrencininbilgileri();
+                        break;
+
+                    case "2":
+                     double ogrencininnotortalaması=   ogrencinin.ogrencinortalaması();
+                        Console.WriteLine("Öğrencinin not ortalaması :"+ogrencininnotortalaması);
+                        break;
+                    case "3":
+                        ogrencinin.okuladii();
+                        break;
+                    case "4":
+                        kontrol=false;
+                        break;
+
+                }
+            }
+            
+            
+            
+
+        }
+
+
+        static void islemleriGöster()
+        {
+            Console.WriteLine("1- Öğrenci bilgilerini göster :");
+            Console.WriteLine("2- Öğrenci ortalamasını göster :");
+            Console.WriteLine("3- Öğrencinin okulu :");
+            Console.WriteLine("4- Çıkış yap");
+        }
+        
+    }
+}
---
+++ ogrenciUygulamasi/ogrencinin.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrenciUygulamasi
+{
+    public class ogrencinin
+    {
+        private int ogrencino;
+        private string isim;
+        private string soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okulismi;
+        
+
+        public ogrencinin(int _ogrencino, string _isim, string _soyisim, int _vize1, int _vize2, int _final, string _okulismi)
+        {
+            ogrencino = _ogrencino;
+            isim = _isim;
+            soyisim = _soyisim;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okulismi = _okulismi;
+            
+            
+        }
+     
+
+        public void ogrencininbilgileri()
+        {
+            Console.WriteLine("Öğrencini adı :"+isim);
+            Console.WriteLine("öğrencinin soyadı :"+soyisim);
+            Console.WriteLine("öğrencinin numarası :"+ogrencino);
+            Console.WriteLine("öğrencinin okul ismi :"+okulismi);
+            Console.WriteLine("öğrencinin vize 1 ortalaması :"+vize1);
+            Console.WriteLine("öğrencinin vize 2 ortalaması :"+vize2);
+            Console.WriteLine("öğrencinin final ortalaması :"+final);
+        }
+
+        public double ogrencinortalaması()
+        {
+            double notortalama = vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+            return notortalama;
+        }
+        
+        public void okuladii()
+        {
+            Console.WriteLine("öğrencinin okul adı :"+okulismi);
+        }
+        
+        
+    }
+}
